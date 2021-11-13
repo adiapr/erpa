824cffb489403a61d5ee3492f4bda389e0a1efff
adiapr <da801100@gmail.com>
2021-11-13 16:24:03 +0700

    Seedeer

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //memasukkan data otomatis ke database
+        DB::table('users')->insert([
+            "name"      => "Administrator",
+            "email"     => "adminstrator@gmail.com",
+            "password"  => bcrypt("administrator"),
+            "role"      => "adminstrator"
+        ]);
+
+        DB::table('users')->insert([
+            "name"      => "asosiasi",
+                "email"     => "asosiasi@gmail.com",
+                "password"  => bcrypt("asosiasi"),
+                "role"      => "asosiasi"
+        ]);
+
+        DB::table('users')->insert([
+            "name"      => "ketua",
+                "email"     => "ketua@gmail.com",
+                "password"  => bcrypt("ketua"),
+                "role"      => "ketua"
+        ]);
+
+        DB::table('users')->insert([
+            "name"      => "hukum",
+                "email"     => "hukum@gmail.com",
+                "password"  => bcrypt("hukum"),
+                "role"      => "hukum"
+        ]);
+
+        DB::table('users')->insert([
+            "name"      => "verifikator",
+                "email"     => "verifikator@gmail.com",
+                "password"  => bcrypt("verifikator"),
+                "role"      => "verifikator"
+        ]);
+    }
+}
