5d38059a2091c9fbac06b42939f13b3364caf535
adiapr <da801100@gmail.com>
2021-11-29 19:12:10 +0700

    Fix Autocomplete

--- resources/views/administrator/tes.php
+++ resources/views/administrator/tes.php
@@ -1,76 +1,114 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--   Core JS Files   -->
+<script src="../../assets/js/core/jquery.3.2.1.min.js"></script>
+<script src="../../assets/js/core/popper.min.js"></script>
+<script src="../../assets/js/core/bootstrap.min.js"></script>
+<!-- jQuery UI -->
+<script src="../../assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+<script src="../../assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-    <!-- Styles -->
-</head>
-<body>
-    <div id="app">
-        <div class="container">
-            <main class="py-4">
-                <div class="form-group mt-10">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nama Pasien') }}</label>
-                    <div class="col-md-6">
-                        <input type="text" id='pasien' class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Puskesmas') }}</label>
-                    <div class="col-md-6">
-                        <input type="text" id='puskesmas' class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Kecamatan') }}</label>
-                    <div class="col-md-6">
-                        <input type="text" id='kecamatan' class="form-control">
-                    </div>
-                </div>
-                @yield('content')
-            </main>
-        </div>
-    </div>
-</body>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E=" crossorigin="anonymous"></script>
-    <script type="text/javascript">
-        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-        $(document).ready(function(){
-            $( "#pasien" ).autocomplete({
-                source: function( request, response ) {
-                    console.log(request.term)
-                $.ajax({
-                    url:"{{route('pasien')}}",
-                    type: 'post',
-                    dataType: "json",
-                    data: {
-                        _token: CSRF_TOKEN,
-                        cari: request.term
-                    },
-                    success: function( data ) {
-                    response( data );
-                    }
-                });
+<!-- jQuery Scrollbar -->
+<script src="../../assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<!-- Datatables -->
+<script src="../../assets/js/plugin/datatables/datatables.min.js"></script>
+<!-- Atlantis JS -->
+<script src="../../assets/js/atlantis.min.js"></script>
+<!-- Atlantis DEMO methods, don't include it in your project! -->
+<script src="../../assets/js/setting-demo2.js"></script>
+{{-- autocomplete --}}
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js" integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+    var cache = {};
+    $(document).ready(function(){
+        $( "#nama_organisasi" ).autocomplete({
+            source: function( request, response ) {
+                var term = request.term;
+                console.log(request.term)
+            $.ajax({
+                url:"{{route('asosiasi.organisasi.autocomplete')}}",
+                type: 'post',
+                dataType: "json",
+                data: {
+                    _token: CSRF_TOKEN,
+                    cari: request.term
                 },
-                select: function (event, ui) {
-                $('#pasien').val(ui.item.label);
-                $('#puskesmas').val(ui.item.faskes);
-                $('#kecamatan').val(ui.item.kecamatan);
-                return false;
+                success: function( data ) {
+                response( data );
                 }
             });
+            },
+            select: function (event, ui) {
+            $('#nama_organisasi').val(ui.item.label);
+            $('#alamat_organisasi').val(ui.item.alamat_organisasi);
+            $('#telp').val(ui.item.telp);
+            return false;
+            }
         });
-  </script>
-</html>
+    });
+</script>
+
+<script >
+    $(document).ready(function() {
+        $('#basic-datatables').DataTable({
+        });
+
+        $('#multi-filter-select').DataTable( {
+            "pageLength": 5,
+            initComplete: function () {
+                this.api().columns().every( function () {
+                    var column = this;
+                    var select = $('<select class="form-control"><option value=""></option></select>')
+                    .appendTo( $(column.footer()).empty() )
+                    .on( 'change', function () {
+                        var val = $.fn.dataTable.util.escapeRegex(
+                            $(this).val()
+                            );
+
+                        column
+                        .search( val ? '^'+val+'$' : '', true, false )
+                        .draw();
+                    } );
+
+                    column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' )
+                    } );
+                } );
+            }
+        });
+
+        // Add Row
+        $('#add-row').DataTable({
+            "pageLength": 5,
+        });
+
+        var action = '<td> <div class="form-button-action"> <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task"> <i class="fa fa-edit"></i> </button> <button type="button" data-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove"> <i class="fa fa-times"></i> </button> </div> </td>';
+
+        $('#addRowButton').click(function() {
+            $('#add-row').dataTable().fnAddData([
+                $("#addName").val(),
+                $("#addPosition").val(),
+                $("#addOffice").val(),
+                action
+                ]);
+            $('#addRowModal').modal('hide');
+
+        });
+    });
+</script>
+<script>
+    function bacaGambar(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#gambar_nodin').attr('src', e.target.result);
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#preview_gambar").change(function(){
+         bacaGambar(this);
+    });
+</script>
